ba2b578a5c602125346ec72bc2835b708ccbf35c
gilhad <gilhad@seznam.cz>
2025-05-22 23:07:50 +0200

    BRANCH offset is in bytes, not in cells

--- version.h
+++ version.h
@@ -1,4 +1,4 @@
 #pragma once
-#define VERSION_STRING "0.0.1-2-g2dea6d4++"
-#define VERSION_COMMIT "2dea6d4"
-#define VERSION_MESSAGE "compiles, crashes"
+#define VERSION_STRING "0.0.1-4-g71cff09++"
+#define VERSION_COMMIT "71cff09"
+#define VERSION_MESSAGE "BRANCH offset is in bytes, not in cells"
